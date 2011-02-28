b075d570996d96f46ca4fd6a8c1e286806c4f63e
Gerd Wachsmuth <gerd.wachsmuth@mathematik.tu-chemnitz.de>
2011-02-28 13:50:36 +0100

    Minor changes in auxoutline.py

--- ftplugin/latex-suite/auxoutline.py
+++ ftplugin/latex-suite/auxoutline.py
@@ -50,13 +50,10 @@ def stripComments(contents):
     return nonempty
 # }}}
 # getSectionLabels_Root {{{
-def getSectionLabels_Root(lineinfo, section_prefix, label_prefix, label_value):
-    inside_env = 0
-    prev_env = ''
+def getSectionLabels_Root(lineinfo, section_prefix, label_prefix, value_prefix):
     outstr = StringIO.StringIO('')
     pres_depth = section_prefix
 
-    #print '+getSectionLabels_Root: lineinfo = [%s]' % lineinfo
     for line in lineinfo.splitlines():
         prev_txt = ''
         if not line:
@@ -72,6 +69,7 @@ def getSectionLabels_Root(lineinfo, section_prefix, label_prefix, label_value):
             # which will be displayed below this label
             n = re.search(r'\\newlabel{(%s.*?)}{{(\\relax )?(.*?)}.*{(aliascounter:)?(.*)}{.*?}}' % label_prefix, line)
             if n:
+              # Version with hyperref
               o = re.search(r'equation\.(.*)', n.group(5))
               if o:
                 # Found an equation
@@ -79,40 +77,35 @@ def getSectionLabels_Root(lineinfo, section_prefix, label_prefix, label_value):
               else:
                 o = re.search(r'AMS\.(.*)', n.group(5))
                 if o:
-                  # Found an named equation
-                  p = re.search(r'\\newlabel{(%s.*?)}{{{(.*?)}}' % label_prefix, line)
+                  # Found an named equation => try to find the name
+                  # After the name, there is the page number (assumed to be of the form [0-9a-zA-Z]* )
+                  p = re.search(r'\\newlabel{(%s.*?)}{{{(.*?)}}{[0-9a-zA-Z]*}' % label_prefix, line)
                   if p:
-                    # Remove all "{" and "}" (for subequtaions)
-                    equation_number = re.sub('[{}]', '',  p.group(2) )
+                    equation_number = p.group(2)
                     prev_txt += '(' + equation_number + ')'
                   else:
                     prev_txt += n.group(5)
                 else:
-                  prev_txt += re.match(r'\w*', n.group(5)).group() + '.' + n.group(3)
+                  prev_txt += re.match(r'^\w*', n.group(5)).group() + '.' + n.group(3)
             else:
+              # Version without hyperref
               n = re.search(r'\\newlabel{(%s.*?)}{{(\\relax )?(.*)}{.*}}' % label_prefix, line)
               prev_txt += n.group(3)
 
-            # print label_value
-            # print prev_txt
-            if re.match( label_value, prev_txt):
+            if re.match( value_prefix, prev_txt):
                 # Print label and counter+number:
                 print >>outstr, '>%s%s' % (' '*(2*pres_depth-2), m.group(1))
                 print >>outstr, ':%s%s' % (' '*(2*pres_depth+0), prev_txt)
 
     return outstr.getvalue()
-    
 # }}}
 # getSectionLabels {{{
 def getSectionLabels(lineinfo, 
         sectypes=['part', 'chapter', 'section', 'subsection', 'subsubsection', 'paragraph', 'subparagraph'], 
-        section_prefix=1, label_prefix='', label_value=''):
+        section_prefix=1, label_prefix='', value_prefix=''):
 
     if not sectypes:
-        return getSectionLabels_Root(lineinfo, section_prefix, label_prefix, label_value)
-
-    ##print 'sectypes[0] = %s, section_prefix = [%s], lineinfo = [%s]' % (
-    ##        sectypes[0], section_prefix, lineinfo)
+        return getSectionLabels_Root(lineinfo, section_prefix, label_prefix, value_prefix)
 
     sections = re.split(r'(\\@writefile{toc}{\\contentsline {%s}.*)' % sectypes[0], lineinfo)
     
@@ -120,28 +113,32 @@ def getSectionLabels(lineinfo,
     # others containing the child sections as paris of [section_name,
     # section_text]
 
-    rettext = getSectionLabels(sections[0], sectypes[1:], section_prefix, label_prefix, label_value)
+    rettext = getSectionLabels(sections[0], sectypes[1:], section_prefix, label_prefix, value_prefix)
  
     for i in range(1,len(sections),2):
-        # print sections[i]
-        o = re.search( r'{%s}{\\numberline {(\\relax )?(.*?)}(.*?)}' % sectypes[0] , sections[i])
-        if o:
-          section_name = o.group(3)
-          sec_num = o.group(2) + ' '
-        else:
-          o = re.search( r'{%s}{(.*?)}' % sectypes[0] , sections[i])
-          section_name = o.group(1)
-          sec_num = ''
 
-        section_label_text = getSectionLabels(sections[i] + sections[i+1], sectypes[1:], 
-                                    section_prefix+1, label_prefix, label_value)
+        section_label_text = getSectionLabels(sections[i+1], sectypes[1:], section_prefix+1, label_prefix, value_prefix)
 
         if section_label_text:
-            sec_heading = 2*' '*(section_prefix-1) # + section_prefix
-            sec_heading += '%s%s' % (sec_num, section_name)
-            sec_heading += '<<<%d\n' % section_prefix
+          # This section contains labels
+          # Let us determine the section number and the section heading
+          o1 = re.search( r'{%s}{\\numberline {(\\relax )?(.*?)}(.*?)}' % sectypes[0] , sections[i])
+          o2 = re.search( r'{%s}{(.*?)}' % sectypes[0] , sections[i])
+          if o1:
+            section_name = o1.group(3)
+            section_number = o1.group(2) + ' '
+          elif o2:
+            section_name = o2.group(1)
+            section_number = ''
+          else:
+            print 'Unknown heading format "%s"' % sections[i]
+            section_name = "Unknown Name"
+            section_number = "??"
+          sec_heading = 2*' '*(section_prefix-1)
+          sec_heading += '%s%s' % (section_number, section_name)
+          sec_heading += '<<<%d\n' % section_prefix
 
-            rettext += sec_heading + section_label_text
+          rettext += sec_heading + section_label_text
 
     return rettext
     
@@ -161,21 +158,31 @@ def main(fname, prefix):
         lineinfo += line + '\n'
 
     
+    # Does prefix look like a label or a value?
     o = re.match( '(\([0-9a-zA-Z.]*|\w*\.[0-9a-zA-Z.]*)' , prefix )
     if o:
         label_prefix = ''
-        label_value = re.escape(prefix)
+        value_prefix = re.escape(prefix)
     else:
         label_prefix = prefix
-        label_value = ''
-    rettext = getSectionLabels(lineinfo, label_prefix=label_prefix, label_value=label_value)
+        value_prefix = ''
+
+    rettext = getSectionLabels(lineinfo, label_prefix=label_prefix, value_prefix=value_prefix)
     a = re.findall( r'(^|\n)> *([^ ]*)\n' , rettext)
 
-    # Only one matching label? => return it
     if len(a) == 0:
         return ''
     elif len(a) == 1:
-        return a[0][1]
+        # Only one partial match
+        # Check, if prefix matches exactly
+        if value_prefix != '' and re.search( r'\n: *%s\n' % value_prefix, rettext):
+          # Value_prefix matches _exactly_ counter.number => return only this matching label
+          return a[0][1]
+        elif label_prefix != '':
+          # Prefix matches the beginning of the label => return only this matching label
+          return a[0][1]
+        else:
+          return rettext
     else:
         return rettext
 # }}}
